acad07fd4c8e8e6ce6664876d9301a6d64101bf5
evb0110 <7149553@gmail.com>
2019-01-25 23:48:42 +0300

    solved

---
+++ src/js/getLevel.test.js
@@ -0,0 +1,25 @@
+import fetchData from './http';
+import getLevel from './getLevel';
+
+jest.mock('./http');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test('testing http request address for userId == 42', () => {
+  fetchData.mockReturnValue({});
+
+  getLevel(42);
+  expect(fetchData).toBeCalledWith('https://server/user/42');
+});
+
+test('testing level value for ok response', () => {
+  const response = {
+    status: 'ok',
+    level: 42,
+  };
+  fetchData.mockReturnValue(response);
+  const level = getLevel(1);
+  expect(level).toMatch(/: 42/);
+});
